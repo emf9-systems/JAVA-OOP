8b7ed7737359a3e210b7597f4777373b0d5d65ff
Angel Olivares <aaommago@proton.me>
2025-01-15 18:42:57 -0300

    Ejercicio creacion de objeto Vehiculo

---
+++ Aplicacion.java
@@ -0,0 +1,41 @@
+public class Aplicacion {
+    
+    public static void main(String[] args) {
+        
+
+
+        // Crear nuevo vehiculo
+
+        Vehiculo auto1 = new Vehiculo();
+
+        auto1.ano = 1996;
+        auto1.marca = "Honda";
+        auto1.modelo = "Civic (Hatchback)";
+        auto1.color = "Verde";
+        auto1.ruedas = 4;
+
+        Vehiculo autoRojo = new Vehiculo("rojo");
+
+        Vehiculo autoCompleto = new Vehiculo(1997, "Suzuki", "Maruti");
+
+        Vehiculo autoCompleto2 = new Vehiculo(2006, "rojo", "Mazda", "RX-8 (coupe)");
+
+        Vehiculo motocicleta = new Vehiculo(2025, "negra", "Yamaha", "YZF-R3", 2);
+
+        System.out.println("auto1 modelo: "+auto1.modelo);
+
+        System.out.println("autoRojo color: "+autoRojo.color);
+
+        System.out.println("autoCompleto color y modelo: "+autoCompleto.color+" "+autoCompleto.modelo);
+
+        System.out.println("autoCompleto2 ruedas, color y modelo: "+autoCompleto2.ruedas+" "+autoCompleto2.color+" "+autoCompleto2.modelo+" ");
+        
+        System.out.println("motocicleta ruedas, modelo y año:"+motocicleta.ruedas+" "+motocicleta.modelo+" "+motocicleta.ano+" ");
+
+
+
+
+
+    }
+
+}
---
+++ Vehiculo.java
@@ -0,0 +1,49 @@
+public class Vehiculo {
+    
+
+    // Atributos del objeto
+    public int ano;
+    public String marca;
+    public String modelo;
+    public String color;
+    public int ruedas;
+
+    // Metodo constructor: encargado de inicializar la instancia
+    public Vehiculo() {
+        // Cree un nuevo vehiculo
+
+
+    }
+    // SOBRECARGA DE METODO CONSTRUCTOR
+    public Vehiculo(String color) {
+        this.color = color;
+    }
+
+    public Vehiculo(int ano, String marca, String modelo) {
+        this.color = "Blanco";
+        this.ano = ano;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ruedas = 4;
+    }
+
+
+    public Vehiculo(int ano, String color, String marca, String modelo) {
+        this.color = color;
+        this.ano = ano;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ruedas = 4;
+    }
+
+
+
+    public Vehiculo(int ano, String color, String marca, String modelo, int ruedas) {
+        this.color = color;
+        this.ano = ano;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ruedas = ruedas;
+    }
+
+}
